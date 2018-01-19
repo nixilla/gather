493c3dd590e24c606af77b7a53bb85e6609c5c62
Obdulia Losantos <obdulia.losantos@gmail.com>
2018-01-19 14:28:10 +0100

    chore: upgrade django@2 (#323)
    * chore: upgrade django@2
    
    * chore: small tweaks
    
    * chore: last aether.common distribution

--- app/gather/api/urls.py
+++ app/gather/api/urls.py
@@ -1,9 +1,8 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.urls import path, include
 
 from rest_framework import routers
-
-from aether.common.conf.urls import url_pattern as url, include
 
 from .decorators import tokens_required
 from . import views
@@ -16,13 +15,13 @@ router.register('surveys', views.SurveyViewSet, base_name='surveys')
 router.register('masks', views.MaskViewSet, base_name='masks')
 
 urlpatterns = [
-    url(r'^gather/', include(router.urls)),
+    path('gather/', include(router.urls)),
 ]
 
 
 for app_name in settings.AETHER_APPS:
     urlpatterns.append(
-        url(r'^{}/(?P<path>.*)$'.format(app_name),
-            login_required(tokens_required(views.TokenProxyView.as_view(app_name=app_name))),
-            name='{}-proxy'.format(app_name))
+        path('{}/<path:path>'.format(app_name),
+             login_required(tokens_required(views.TokenProxyView.as_view(app_name=app_name))),
+             name='{}-proxy'.format(app_name))
     )
--- app/gather/urls.py
+++ app/gather/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 
-from aether.common.conf.urls import generate_urlpatterns, url_pattern as url, include
+from aether.common.conf.urls import generate_urlpatterns
 
 # Any entry here needs the decorator `tokens_required` if it's going to execute
 # AJAX request to any of the other apps
@@ -14,32 +15,31 @@ urlpatterns = generate_urlpatterns(kernel=True) + [
 
     # ----------------------
     # API
-    url(r'', include('gather.api.urls')),
-    url(r'^v1/', include('gather.api.urls', namespace='v1')),
+    path('', include('gather.api.urls')),
+    path('v1/', include('gather.api.urls', namespace='v1')),
 
     # ----------------------
     # Welcome page
-    url(r'^$',
-        login_required(TemplateView.as_view(template_name='pages/index.html')),
-        name='index-page'),
+    path('',
+         login_required(TemplateView.as_view(template_name='pages/index.html')),
+         name='index-page'),
 
     # ----------------------
     # shows the current user app tokens
-    url(r'^~tokens$',
-        login_required(TemplateView.as_view(template_name='pages/tokens.html')),
-        name='tokens'),
+    path('~tokens',
+         login_required(TemplateView.as_view(template_name='pages/tokens.html')),
+         name='tokens'),
     # to check if the user tokens are valid
-    url(r'^check-tokens$', login_required(tokens_required(empty)), name='check-tokens'),
+    path('check-tokens', login_required(tokens_required(empty)), name='check-tokens'),
 
-    url(r'^surveys/(?P<action>\w+)/(?P<survey_id>[0-9]+)?$',
-        login_required(tokens_required(TemplateView.as_view(template_name='pages/surveys.html'))),
-        name='surveys'),
-
+    re_path(r'^surveys/(?P<action>\w+)/(?P<survey_id>[0-9a-f-]+)?$',
+            login_required(tokens_required(TemplateView.as_view(template_name='pages/surveys.html'))),
+            name='surveys'),
 ]
 
 if settings.AETHER_ODK:  # pragma: no cover
     urlpatterns += [
-        url(r'^surveyors/(?P<action>\w+)/(?P<surveyor_id>[0-9]+)?$',
-            login_required(tokens_required(TemplateView.as_view(template_name='pages/surveyors.html'))),
-            name='surveyors'),
+        re_path(r'^surveyors/(?P<action>\w+)/(?P<surveyor_id>[0-9]+)?$',
+                login_required(tokens_required(TemplateView.as_view(template_name='pages/surveyors.html'))),
+                name='surveyors'),
     ]
