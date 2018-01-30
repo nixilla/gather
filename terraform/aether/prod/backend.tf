7e54a90e913be834cd2d6d510b80193958936943
MrPink <will.pink@gmail.com>
2018-01-30 16:14:45 +0100

    chore: not required

--- terraform/aether/prod/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-        bucket = "eha-prod-state-files"
-        key = "gather-on-aether-prod/terraform.tfstate"
-        region = "eu-west-1"
-        dynamodb_table = "gather-on-aether-prod-terraform-lock"
-  }
-}
--- terraform/aether/prod/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "environment" { default = "prod" }
-variable "project" { default = "aether" }
-variable "aws_region" { default = "eu-west-1" }
-variable "project_billing_id" { default = "internal" }
-
-# RDS
-variable "db_engine_type" { default="postgres" }
-variable "db_engine_version" { default="9.5.4" }
