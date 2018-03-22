dbd45f86d84a3eb4f57ab7d7c74d2cea40ec61a4
William Pink <will.pink@gmail.com>
2018-03-22 12:02:12 +0100

    Chore: add deployer (#370)
    * chore: comments
    
    * chore: add AWS keys
    
    * chore: build on RC's and tags
    
    * chore: build on Travis
    
    * chore: add ECR
    
    * chore: DOCKER_TAG
    
    * chore: update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,49 +2,29 @@
 set -e
 
 export AWS_DEFAULT_REGION="eu-west-1"
-COMMIT="${TRAVIS_COMMIT}"
-BRANCH="${TRAVIS_BRANCH}"
-IMAGE_REPO="387526361725.dkr.ecr.eu-west-1.amazonaws.com"
+export BRANCH="${TRAVIS_BRANCH}"
+export DOCKER_REPO="387526361725.dkr.ecr.eu-west-1.amazonaws.com"
+export APP="gather"
 
-if [ "${BRANCH}" == "gather-on-aether" ]; then
-  export APP=( gather )
-  export ENV="dev"
+$(aws ecr get-login --no-include-email)
+
+if [[ -n $TRAVIS_TAG ]]; then
+  DOCKER_TAG=`cat ../VERSION`
+else
+  DOCKER_TAG=$TRAVIS_BRANCH
 fi
 
-$(aws ecr get-login --region="${AWS_DEFAULT_REGION}" --no-include-email)
-
-TOKEN="deploy_token_${ENV}"
-
-function install_kubectl {
-  curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-  chmod +x ./kubectl
-  sudo mv ./kubectl /usr/local/bin/kubectl
-  mkdir -p ~/.kube
-  openssl aes-256-cbc -K $encrypted_176cb6ce0b2f_key -iv $encrypted_176cb6ce0b2f_iv \
-  -in conf/${ENV}_kube_config.enc -out ~/.kube/config -d
-}
-
-# authenticate to the K8's cluster
-function configure_kubectl {
-  install_kubectl
-  kubectl proxy --port=8080 &
-}
-
-GATHER2_APP="gather-on-aether"
+# Build and push docker
 docker-compose build $APP
+docker tag $APP ${DOCKER_REPO}/${APP}:${DOCKER_TAG}
+docker push ${DOCKER_REPO}/${APP}:${DOCKER_TAG}
 
-echo "Building Docker image ${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${BRANCH}"
-docker tag $APP "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${BRANCH}"
-docker tag $APP "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${COMMIT}"
-docker tag $APP "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:latest"
-
-echo "Pushing Docker image ${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${BRANCH}"
-docker push "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${BRANCH}"
-docker push "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${COMMIT}"
-docker push "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:latest"
-echo "Configure Kubectl"
-configure_kubectl
-
-echo "Deploying ${APP} to ${ENV}"
-kubectl set image deployment/gather ${APP}=${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${COMMIT}
-kubectl rollout status deployment/${APP}
+# # Publish Helm package
+# docker run -a STDOUT -a STDERR \
+#  -e BRANCH=$TRAVIS_BRANCH -e RELEASE=$RELEASE \
+#  -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+#  -e APP=$APP \
+#  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+#  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+#  ehealthafrica/deployer \
+#  publish_helm
---
+++ scripts/terraform/ecr.tf
@@ -0,0 +1,5 @@
+# ECR repository
+
+resource "aws_ecr_repository" "ecr" {
+  name = "gather"
+}
