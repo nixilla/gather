bd87ecebea8edb217fc80f0266b9f9b0e440d866
obdulia <obdulia.losantos@gmail.com>
2020-01-21 14:20:05 +0100

    chore: split release and deployment stages

--- scripts/release.sh
+++ scripts/release.sh
@@ -31,31 +31,6 @@ function build_container {
         ${container}
 }
 
-function docker_push {
-    local TAG=$1
-    local IMAGE_REPO=$2
-    local IMAGE=${IMAGE_REPO}/${APP}:${TAG}
-    local IMAGE_SHA=${IMAGE_REPO}/${APP}:${TRAVIS_COMMIT}
-    local PUSH_SHA=$3
-
-    echo "Pushing Docker image ${IMAGE}"
-    docker tag ${APP} ${IMAGE}
-    docker push ${IMAGE}
-
-    if [[ $PUSH_SHA == 'true' ]]; then
-        echo "Pushing Docker image ${IMAGE_SHA}"
-        docker tag ${APP} ${IMAGE_SHA}
-        docker push ${IMAGE_SHA}
-    fi
-}
-
-if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
-    echo "--------------------------------------------------------------"
-    echo "Skipping a release because this is a pull request: ${TRAVIS_PULL_REQUEST}"
-    echo "--------------------------------------------------------------"
-    exit 0
-fi
-
 # release version depending on TRAVIS_BRANCH (develop | release-#.#) / TRAVIS_TAG (#.#.#)
 if [[ ${TRAVIS_TAG} =~ ^[0-9]+(\.[0-9]+){2}$ ]]; then
     VERSION=${TRAVIS_TAG}
@@ -63,16 +38,10 @@ if [[ ${TRAVIS_TAG} =~ ^[0-9]+(\.[0-9]+){2}$ ]]; then
 elif [[ ${TRAVIS_BRANCH} =~ ^release\-[0-9]+\.[0-9]+$ ]]; then
     VERSION=`cat VERSION`
     # append "-rc" suffix
-    VERSION=${VERSION}-rc
-
-elif [[ ${TRAVIS_BRANCH} = "develop" ]]; then
-    VERSION="alpha"
+    VERSION="${VERSION}-rc"
 
 else
-    echo "--------------------------------------------------------------"
-    echo "Skipping a release because this branch is not permitted: ${TRAVIS_BRANCH}"
-    echo "--------------------------------------------------------------"
-    exit 0
+    VERSION="alpha"
 fi
 
 echo "--------------------------------------------------------------"
@@ -82,6 +51,7 @@ echo "Release revision:    ${TRAVIS_COMMIT}"
 echo "--------------------------------------------------------------"
 
 APP="gather"
+DOCKER_IMAGE="ehealthafrica/${APP}:${VERSION}"
 
 # Build and distribute the JS assets
 build_container gather-assets
@@ -90,32 +60,11 @@ docker-compose run --rm gather-assets build
 # Build and push docker image to docker hub
 build_container ${APP}
 
-if [[ ${VERSION} = "alpha" ]]; then
-    # ===========================================================
-    # push images to deployment repository
+# Login in docker hub
+docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASSWORD}
 
-    GCR_REPO_URL="https://eu.gcr.io"
-    IMAGE_REPO="eu.gcr.io/development-223016"
-
-    openssl aes-256-cbc -K $encrypted_422343ef1cd5_key -iv $encrypted_422343ef1cd5_iv -in gcs_key.json.enc -out gcs_key.json -d
-    docker login -u _json_key -p "$(cat gcs_key.json)" $GCR_REPO_URL
-    docker_push ${VERSION} ${IMAGE_REPO} true
-
-    export GOOGLE_APPLICATION_CREDENTIALS=gcs_key.json
-    push-app-version --project gather-alpha --version $TRAVIS_COMMIT
-    docker logout
-
-    # Login in docker hub
-    docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASSWORD}
-    docker_push ${VERSION} ehealthafrica false
-else
-    # Login in docker hub
-    docker login -u ${DOCKER_HUB_USER} -p ${DOCKER_HUB_PASSWORD}
-
-    docker_push ${VERSION} ehealthafrica
-    if [ -z "$TRAVIS_TAG" ]; then
-        docker_push "${VERSION}--${TRAVIS_COMMIT}" ehealthafrica false
-    fi
-fi
+echo "Pushing Docker image ${DOCKER_IMAGE}"
+docker tag ${APP} ${DOCKER_IMAGE}
+docker push ${DOCKER_IMAGE}
 
 docker logout
